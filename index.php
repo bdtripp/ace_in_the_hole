9c066688ec92b19121471ba13ae6959db3718777
Brian Tripp <brian.tripp@pcc.edu>
2020-06-23 14:19:51 -0700

    added Google Tag Manager

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ require_once("includes/form_processing.php");
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5XF983C');</script>
+        <!-- End Google Tag Manager -->
+        
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135450898-2"></script>
         <script>
@@ -27,6 +35,11 @@ require_once("includes/form_processing.php");
         <script src="image_slider.js"></script>
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XF983C"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        
         <div id="fb-root"></div>
         <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
         <?php include 'includes/header.php'; ?>
